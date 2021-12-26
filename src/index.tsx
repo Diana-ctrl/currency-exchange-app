0c7f81c8a764c8bd53e51aa4d4d957a13646d98f
Diana <diana.lyah.96@mail.ru>
2021-12-26 20:04:31 +0300

    added presentation and container component

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CurrencyExchangeContainer from './CurrencyExchangeContainer';
 
-export type initialStateType = {
-  playlists: Array<string>
+const Lesson1: React.FunctionComponent = () => {
+    return (
+    <React.Fragment>
+        <CurrencyExchangeContainer />
+    </React.Fragment>
+  );
 };
-export type AppStateType = ReturnType<typeof rootReducer>
-type actionType = {
-  type: 'ADD_TRACK',
-  payload: string
-}
 
-const initialState: initialStateType = {
-  playlists: ['Smalls like spirit', 'Enter Sandman']
-}
+export default Lesson1;
 
-function playlistReducer (state:initialStateType = initialState, action: actionType):initialStateType {
-  if (action.type === 'ADD_TRACK') {
-    return {
-      ...state,
-      playlists: [...state.playlists, action.payload]
-  }
-  }
-  return state;
-}
-
-let rootReducer = combineReducers({
-  playList: playlistReducer
-})
-
-let store = createStore(rootReducer);
- 
-ReactDOM.render(
-  <Provider store={store}>
-      <App />
-  </Provider>,
-  document.getElementById('root')
-);
-reportWebVitals();
